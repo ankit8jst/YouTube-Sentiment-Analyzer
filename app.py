ec71e2b805a75ef7f73d506ca746ef5dd93fe2af
ankit8jst <147655585+ankit8jst@users.noreply.github.com>
2025-04-02 19:53:16 +0530

    app.py

--- app.py
+++ app.py
@@ -125,5 +125,6 @@ def result():
         top_negative=top_negative
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000, debug=True)
+
